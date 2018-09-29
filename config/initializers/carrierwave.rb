e8c11b5e74b3f18f5e10a88094f7bfcf84e12257
240Yoshihiro <qwe.ima.poi@docomo.ne.jp>
2018-09-29 14:31:00 +0900

    fix carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -13,5 +13,5 @@ CarrierWave.configure do |config|
   }
 
   config.fog_directory  = 'fuyoufile'
-  config.asset_host = 'https://s3-ap-northeast-1'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/fuyoufile'
 end
